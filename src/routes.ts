612291e5174e27ca8595acf6041c799bf77c2658
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-05-16 11:50:43 -0300

    Using the Books Controller class

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { BooksController } from "./controllers/BooksController";
 
 const routes = Router();
+
+const booksController = new BooksController();
+
+routes.post("/search", booksController.create);
 
 export { routes };
