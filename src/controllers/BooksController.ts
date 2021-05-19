5d71fe87021b57f70ed46014139c38d6e3920832
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-05-18 22:46:25 -0300

    Returning only the data required to the client

--- src/controllers/BooksController.ts
+++ src/controllers/BooksController.ts
@@ -2,13 +2,37 @@ import { Request, Response } from "express";
 import { BooksService } from "../services/BooksServices";
 
 class BooksController {
-  async create(request: Request, response: Response) {
+  async create(request: Request, response: Response): Promise<Response> {
     const { search_value } = request.body;
     const booksService = new BooksService();
+    const { items } = await booksService.getBooks(search_value);
 
-    const resultados = await booksService.getBooks(search_value);
+    let booksReturned = [];
 
-    return response.json(resultados);
+    for (let i = 0; i < items.length; i++) {
+      const book = items[i];
+      const volumeInfo = book.volumeInfo;
+
+      let id = book.id;
+      let thumbnail = "";
+      let { title, description, publishedDate } = volumeInfo;
+
+      if ("imageLinks" in volumeInfo) {
+        thumbnail = volumeInfo.imageLinks.thumbnail;
+      };
+
+      const resume = {
+        id: id,
+        thumbnail: thumbnail,
+        title: title,
+        description: description,
+        publishedDate: publishedDate
+      }
+
+      booksReturned.push(resume);
+
+    }
+    return response.send(JSON.stringify(booksReturned));
   }
 }
 
